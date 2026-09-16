be752b5a95241ba3cd049e7396d7e16db975eb5c
Nick Montana <59708036+nick-montana@users.noreply.github.com>
2026-09-15 21:31:34 -0400

    Update dynatrace.tsx
    Signed-off-by: Nick Montana <59708036+nick-montana@users.noreply.github.com>

--- src/types/dynatrace.tsx
+++ src/types/dynatrace.tsx
@@ -1,14 +1,47 @@
 
+interface MonitoringDashboard {
+    Source: string;
+    Environment: string;
+    DashboardType: string;
+    DashboardUrl: string;
+}
 
+
+interface AssetMonitoring {
+    PerformanceMonitoring: MonitoringDashboard[];
+    ResourceMonitoring: MonitoringDashboard[];
+    DistributedTracing: MonitoringDashboard[];
+
+}
 
 interface DeployableAsset {
-    GitRepoName: string
-    ApplicationName: string
-    Namespace: string
-    Environment: string;
+    GitRepoName: string // github
+    ApplicationName: string // multiple sources
+    Namespace: string // OpenShift/AWS/Azure
+    Environment: string; // Multiple Sources
+    BusinessCriticality: string // servicenow
+    Description: string; // servicenow
     ServiceNowAS: string; // servicenow
-    ManagedBy: string;
-    Domain: string;
-    JiraTeamKey: string
-    WorkloadType: string;
+    ManagedBy: string; // servicenow
+    Domain: string; // servicenow
+    JiraTeamKey: string // github
+    Language: string // multiple sources
+    WorkloadType: string; // multiple sources
+    AlertExists: boolean; // servicenow & monitoring platform
+    Tags: string[]; // multiple sources
+    AssetMonitoring: AssetMonitoring
+}
+
+
+interface SingleLevelConsumerDependency{
+    TracingProvider: string; // monitoring platform
+    Target: string; // dependency deployable asset
+    Source: string; // consumer deployable asset
+}
+
+interface MultiLevelConsumerDependency {
+    TracingProvider: string; // monitoring platform
+    RootConsumerName: string;
+    Target: string; // dependency deployable asset
+    Source: string; // consumer deployable asset
 }
